728ee99e03b4cb4fe4ef9766ac355796123100d4
PTW-ARE <pothiwat.ptw@gmail.com>
2024-10-14 14:06:19 +0700

    Edit

--- cypress/e2e/testFinal/test.cy.js
+++ cypress/e2e/testFinal/test.cy.js
@@ -2,19 +2,38 @@ describe('shop', () => {
 
     beforeEach(()=> {
         cy.viewport(1500, 1500)
-        cy.visit('https://www.shopist.io')
+        cy.visit('https://petstore.octoperf.com/actions/Account.action?signonForm=')
     })
 
-    it('tc-01', () => {
+    //const testDataProfile = require("../../fixtures/profile.json")
+
+    it('TC-Edit-Account', () => {
     
         cy.wait(3000)
-        cy.get('.sofas > .menu-item-large-container > .menu-item-large').click()
+        cy.get('.profile > .menu-item-large-container > .menu-item-large').click()
         cy.wait(5000)
-        cy.get(':nth-child(7) > :nth-child(1) > .product-card > img').click()
-        cy.wait(5000)
-        cy.get('.modal-title').should('have.text','Oops! This item is sold out.')
+        cy.get('.button').click()
         cy.wait(3000)
-        cy.get('.modal-button').click()
+        cy.get('#firstname').clear()
+        cy.get('#lastname').clear()
+        cy.get('#address1').clear()
+        cy.get('#address2').clear()
+        cy.get('#addressCity').clear()
+        cy.get('#addressZipcode').clear()
+        cy.get('#phone').clear()
+        cy.shop(
+            testDataProfile.firstname, 
+            testDataProfile.lastname, 
+            testDataProfile.address1, 
+            testDataProfile.address2, 
+            testDataProfile.addressCity, 
+            testDataProfile.addressZipcode, 
+            testDataProfile.phone
+        )
+        
+        cy.get('.inverted').click()
+        cy.get('.inline-link').click()
+        cy.get('section.profile > .profile > :nth-child(1) > :nth-child(2)').should('have.text','Pothiwat Chaisena')
     })
 
 
